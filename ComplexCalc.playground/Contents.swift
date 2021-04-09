bdc28bfcadf6b1f6c0d7184dc813feba3e9ba989
limj27 <limjustin2@gmail.com>
2021-04-08 19:31:11 -0700

    Finished complexcalc

--- ComplexCalc.playground/Contents.swift
+++ ComplexCalc.playground/Contents.swift
@@ -3,13 +3,77 @@ print("Welcome back to the UW Calculator")
 // Your job is to fill out Calculator so all the expressions
 // below both compile and return "true"
 class Calculator {
-    
+    func add(lhs left: Int, rhs right: Int) -> Int {
+        return left + right
+    }
+    func subtract(lhs left: Int, rhs right: Int) -> Int {
+        return left - right
+    }
+    func multiply(lhs left: Int, rhs right: Int) -> Int {
+        return left * right
+    }
+    func divide(lhs left: Int, rhs right: Int) -> Int {
+        return left / right
+    }
+    func mathOp(lhs left: Int, rhs right: Int, op function: (Int, Int) -> Int) -> Int {
+        return function(left, right)
+    }
+    func add(_ args : [Int]) -> Int{
+        var total = 0
+        for i in args {
+            total += i
+        }
+        return total
+    }
+    func multiply(_ args : [Int]) -> Int{
+        var total = 1
+        for i in args {
+            total *= i
+        }
+        return total
+    }
+    func count(_ args : [Int]) -> Int {
+        return args.count
+    }
+    func avg(_ args : [Int]) -> Int {
+        var total = 0
+        for i in args {
+            total += i
+        }
+        return total / args.count
+    }
+    func mathOp(args arr: [Int], beg x: Int, op function: (Int, Int) -> Int) -> Int{
+        var total = x
+        for i in arr {
+            total = function(total, i)
+        }
+        return total
+    }
+    func add(lhs left: (Int, Int), rhs right: (Int, Int)) -> (Int, Int){
+        let x = left.0 + right.0
+        let y = left.1 + right.1
+        return (x, y)
+    }
+    func subtract(lhs left: (Int, Int), rhs right: (Int, Int)) -> (Int, Int){
+        let x = left.0 - right.0
+        let y = left.1 - right.1
+        return (x, y)
+    }
+    func add(lhs left: [String : Int], rhs right: [String : Int]) -> [String : Int]{
+        let x = left["x"]! + right["x"]!
+        let y = left["y"]! + right["y"]!
+        return ["x" : x, "y" : y]
+    }
+    func subtract(lhs left: [String : Int], rhs right: [String : Int]) -> [String : Int]{
+        let x = left["x"]! - right["x"]!
+        let y = left["y"]! - right["y"]!
+        return ["x" : x, "y" : y]
+    }
 }
 
 let calc = Calculator()  // Don't change this declaration name; it's used in all the tests below
 
 // ====> Add your own tests here if you wish <====
-
 
 // ====> Do not modify code in this section <====
 calc.add(lhs: 2, rhs: 2) == 4
@@ -17,7 +81,7 @@ calc.subtract(lhs: 2, rhs: 2) == 0
 calc.multiply(lhs: 2, rhs: 2) == 4
 calc.divide(lhs: 2, rhs: 2) == 1
 
-calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rjs) + (lhs * rhs) }) == 35
+calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rhs) + (lhs * rhs) }) == 35
     // This style is one way of writing an anonymous function
 calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20
     // This is the second, more terse, style; either works
